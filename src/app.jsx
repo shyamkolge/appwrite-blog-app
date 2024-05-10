1b39c12b3bc2f052dc35c9aca6bf48211fa0eb38
Shyam <shyamnkolge2020@gmail.com>
2024-05-10 12:56:54 +0530

    first commit

---
+++ src/app.jsx
@@ -0,0 +1,10 @@
+import { useState } from "preact/hooks";
+import preactLogo from "./assets/preact.svg";
+import viteLogo from "/vite.svg";
+import "./app.css";
+
+export function App() {
+  const [count, setCount] = useState(0);
+
+  return <h1 className="text-black underline">Hello</h1>;
+}
