dc2cd928107d141c14175797c0e00172741946a2
Shyam <shyamnkolge2020@gmail.com>
2024-05-10 20:23:01 +0530

    file upload, delete and preview service added.

--- src/appwrite/config.js
+++ src/appwrite/config.js
@@ -94,6 +94,34 @@ export class Service {
   }
 
   // # File upload Service
+
+  async uplaodFile(file) {
+    try {
+      return await this.storage.createFile(
+        conf.appwriteBucketId,
+        ID.unique(),
+        file
+      );
+    } catch (error) {
+      console.log("Appwrite :: config :: uplaodFile :: error :: ", error);
+      return false;
+    }
+  }
+
+  async deleteFile(fileId) {
+    try {
+      await this.storage.deleteFile(conf.appwriteBucketId, fileId);
+
+      return true;
+    } catch (error) {
+      console.log("Appwrite :: config :: deleteFile :: error :: ", error);
+      return false;
+    }
+  }
+
+getFilePreview(fileId) {
+    return this.storage.getFilePreview(conf.appwriteBucketId, fileId);
+  }
 }
 
 const service = new Service();
