b0c991d19813e219ed10025b0d8b3e96d11e7f15
shyam <shyamnkolge2020@gmail.com>
2024-06-03 21:21:34 +0530

    post form created

--- src/conponents/index.js
+++ src/conponents/index.js
@@ -3,11 +3,25 @@ import Header from "./Header/Header";
 import Container from "./container/container";
 import Logo from "./Header/utils/Logo";
 import LogoutBtn from "./Header/utils/LogoutBtn";
-import Button  from "./customContainers/Button";
+import Button from "./customContainers/Button";
 import Input from "./customContainers/Input";
-import PostCard from './customContainers/PostCard'
+import PostCard from "./customContainers/PostCard";
 import Login from "./authentication/Login";
 import SignUp from "./authentication/SignUp";
+import CustomEditor from "./customContainers/CustomEditor";
+import Select  from "./customContainers/Select";
 
-
-export { Header, Footer, Container, Logo, LogoutBtn, Button, Input, PostCard, Login, SignUp };
+export {
+  Header,
+  Footer,
+  Container,
+  Logo,
+  LogoutBtn,
+  Button,
+  Input,
+  PostCard,
+  Login,
+  SignUp,
+  CustomEditor,
+  Select
+};
---
+++ src/conponents/post-form/PostForm.jsx
@@ -0,0 +1,40 @@
+import React, { useCallback } from "react";
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import appWriteService from "../../appwrite/config";
+import { useForm } from "react-hook-form";
+import { Button, Input, Select } from "../index";
+
+const PostForm = ({ post }) => {
+  const { register, handleSubmit, watch, setValue, control, getValues } =
+    useForm({
+      defaultValues: {
+        title: post?.title || "",
+        slug: post?.slug || "",
+        content: post?.content || "",
+        status: post?.status || "",
+      },
+    });
+
+  const navigate = useNavigate();
+  const userData = useSelector((state) => state.user.userData);
+
+  const submit = async (data) => {
+
+      if(post){
+          
+        const file = await data.image[0] ? appWriteService.uplaodFile(data.image[0]) : null; 
+
+
+
+
+      }
+
+
+
+  };
+
+  return <div>PostForm</div>;
+};
+
+export default PostForm;
