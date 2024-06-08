1ae227a19777d207425c65615113ba4f1ef6e519
shyam <shyamnkolge2020@gmail.com>
2024-06-08 15:46:14 +0530

    edit post created

---
+++ src/pages/EditPost.jsx
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from "react";
+import { Container, PostForm } from "../conponents";
+import appWriteService from "../appwrite/config";
+import { useNavigate, useParams } from "react-router-dom";
+
+const EditPost = () => {
+  const [post, setPost] = useState(null);
+  const { slug } = useParams();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (slug) {
+      appWriteService.getPost(slug).then((post) => {
+        if (post) {
+          setPost(post);
+        }
+      });
+    } else {
+      navigate("/");
+    }
+  }, [slug, navigate]);
+
+  return post ? (
+    <div>
+      <Container>
+        <PostForm post={post} />
+      </Container>
+    </div>
+  ) : null;
+};
+
+export default EditPost;
